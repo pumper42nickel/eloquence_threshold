9751f9ce2276b56bf2bc4fa3dbdde5f8b0918802
Mohamed00 <malhajamy@gmail.com>
2020-09-26 11:51:32 -0400

    Removed anticrash_res completely and integrated the expressions contained in it into english_fixes for consistency, as all of anticrash_res was an English fix and it didn't make sense for it to affect other languages. Also added an anticrash expression for hesiron.

--- eloquence.py
+++ eloquence.py
@@ -16,26 +16,24 @@ import unicodedata
 
 minRate=40
 maxRate=150
-anticrash_res = {
- re.compile(r'\b(.*?)c(ae|\xe6)sur(e)?', re.I): r'\1seizur',
- re.compile(r"\b(|\d+|\W+)h'(r|v)[e]", re.I): r"\1h \2e",
- re.compile(r"\b(\w+[bdfhjlmnqrvz])(h[he]s)([abcdefghjklmnopqrstvwy]\w+)\b", re.I): r"\1 \2\3",
- re.compile(r"(\d):(\d\d[snrt][tdh])", re.I): r"\1 \2",
- re.compile(r"\b([bcdfghjklmnpqrstvwxz]+)'([bcdefghjklmnprstvwxz']+)'([drtv][aeiou]?)", re.I): r"\1 \2 \3",
- re.compile(r"\b(you+)'(re)+'([drv]e?)", re.I): r"\1 \2 \3",
- re.compile(r"(re|un|non|anti)cosp", re.I): r"\1kosp",
- re.compile(r"(EUR[A-Z]+)(\d+)", re.I): r"\1 \2",
- re.compile(r"\b(\d+|\W+|[bcdfghjklmnpqrstvwxz]+)?t+z[s]che", re.I): r"\1tz sche",
- re.compile(r"\b(juar[aeou]s)([aeiou]{6,})", re.I): r"\1 \2",
-# Does not occur in normal use, however if a dictionary entry contains the Mc prefix, and NVDA splits it up, the synth will crash.
- re.compile(r"\b(Mc) {0,}([A-Z])"): r"\1\2"
-}
-
 pause_re = re.compile(r'([a-zA-Z])([.(),:;!?])( |$)')
 time_re = re.compile(r"(\d):(\d+):(\d+)")
 english_fixes = {
 re.compile(r'(\w+)\.([a-zA-Z]+)'): r'\1 dot \2',
 re.compile(r'([a-zA-Z0-9_]+)@(\w+)'): r'\1 at \2',
+#Does not occur in normal use, however if a dictionary entry contains the Mc prefix, and NVDA splits it up, the synth will crash.
+re.compile(r"\b(Mc)\s+([A-Z][a-z]+)"): r"\1\2",
+re.compile(r'\b(.*?)c(ae|\xe6)sur(e)?', re.I): r'\1seizur',
+re.compile(r"\b(|\d+|\W+)h'(r|v)[e]", re.I): r"\1h \2e",
+re.compile(r"\b(\w+[bdfhjlmnqrvz])(h[he]s)([abcdefghjklmnopqrstvwy]\w+)\b", re.I): r"\1 \2\3",
+re.compile(r"\b(\w+[bdfhjlmnqrvz])(h[he]s)(iron+[degins]?)", re.I): r"\1 \2\3",
+re.compile(r"(\d):(\d\d[snrt][tdh])", re.I): r"\1 \2",
+re.compile(r"\b([bcdfghjklmnpqrstvwxz]+)'([bcdefghjklmnprstvwxz']+)'([drtv][aeiou]?)", re.I): r"\1 \2 \3",
+re.compile(r"\b(you+)'(re)+'([drv]e?)", re.I): r"\1 \2 \3",
+re.compile(r"(re|un|non|anti)cosp", re.I): r"\1kosp",
+re.compile(r"(EUR[A-Z]+)(\d+)", re.I): r"\1 \2",
+re.compile(r"\b(\d+|\W+|[bcdfghjklmnpqrstvwxz])?t+z[s]che", re.I): r"\1tz sche",
+re.compile(r"\b(juar[aeou]s)([aeiou]{6,})", re.I): r"\1 \2"
 }
 french_fixes = {
 re.compile(r'([a-zA-Z0-9_]+)@(\w+)'): r'\1 arobase \2',
@@ -172,7 +170,6 @@ class SynthDriver(synthDriverHandler.SynthDriver):
   #this converts to ansi for anticrash. If this breaks with foreign langs, we can remove it.
   #text = text.encode('mbcs')
   text = normalizeText(text)
-  text = resub(anticrash_res, text)
   if not self._backquoteVoiceTags:
    text=text.replace('`', ' ')
   text = "`vv%d %s" % (self.getVParam(_eloquence.vlm), text) #no embedded commands
